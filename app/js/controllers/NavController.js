a2abbc2d3de35b335ae12cd90c14b850831801c8
Vitaly Gashock <gshockv@gmail.com>
2016-01-13 13:09:56 +0200

    actual cv

--- app/js/controllers/NavController.js
+++ app/js/controllers/NavController.js
@@ -4,9 +4,6 @@ webSiteApp.controller('NavController',
 	function NavController($scope, $location) {
 		$scope.routeIs = function(routeName) {
 			var loc = $location.path();
-			if (loc.indexOf('blog') != -1 && routeName.indexOf('blog') != -1) {
-				return true;
-			}
 			return loc === routeName;	
 		};
 	}
