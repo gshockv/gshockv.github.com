77de3a2f966c9e0a2cb4ca96f7424c7d6b915a40
Vitaly Gashock <gshockv@gmail.com>
2016-01-13 12:18:32 +0200

    home fix

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -2,14 +2,5 @@
 
 webSiteApp.controller('HomeController',
 	function HomeController($scope, $http) {
-		$scope.posts = [];
-
-		$scope.fetch = function() {
-			$http.get('app/blog-content/blog-posts.json').success(function(data, status) {
-				$scope.posts = _.first(data, 2);
-			});
-		};
-
-		$scope.fetch();
 	}
 );
